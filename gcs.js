53f3c5df5c0f225565a900f2ec6cef5584f3bc5c
Tyler Johnson <mail@tyler-johnson.ca>
2016-09-07 21:41:43 -0400

    Use rise-common-electron

--- gcs.js
+++ gcs.js
@@ -1,5 +1,8 @@
+const network= require("rise-common-electron").network;
+
 module.exports = {
   getFileContents() {
-    return require("node-fetch")("http://www.google.com");
+    //return require("node-fetch")("http://www.google.com");
+    return network.httpFetch("http://www.google.com");
   }
 };
